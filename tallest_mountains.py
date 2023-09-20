0f479608ddcc5b2c40f281e2cd96f6b0144d5836
Walter Conrad <waltconrad9@gmail.com>
2023-09-19 21:30:20 -0700

    fixed deprecated error for pandas read_html, added python version file

--- tallest_mountains.py
+++ tallest_mountains.py
@@ -1,3 +1,4 @@
+from io import StringIO
 import requests
 
 import streamlit as st
@@ -23,7 +24,7 @@ soup = BeautifulSoup(response.text,'html.parser')
 asia_html = soup.find_all('table')[2]
 
 # convert to pandas df
-asia_list = pd.read_html(str(asia_html))
+asia_list = pd.read_html(StringIO(asia_html))
 asia_df = pd.DataFrame(asia_list[0]).head(NUM_PER_REGION)
 
 asia_data = asia_df.values.tolist()
@@ -36,7 +37,7 @@ response = requests.get(AFRICA_URL)
 soup = BeautifulSoup(response.text, 'html.parser')
 africa_html = soup.find_all('table')[2]
 
-africa_list = pd.read_html(str(africa_html))
+africa_list = pd.read_html(StringIO(africa_html))
 africa_df = pd.DataFrame(africa_list[0]).head(NUM_PER_REGION)
 africa_data = africa_df.values.tolist()
 
@@ -47,7 +48,7 @@ response = requests.get(ANDES_URL)
 soup = BeautifulSoup(response.text, 'html.parser')
 andes_html = soup.find_all('table')[1]
 
-andes_list = pd.read_html(str(andes_html))
+andes_list = pd.read_html(StringIO(andes_html))
 andes_df = pd.DataFrame(andes_list[0]).drop(['Image'], axis=1).dropna().head(NUM_PER_REGION)
 andes_data = andes_df.values.tolist()
 
@@ -58,7 +59,7 @@ response = requests.get(NORTH_AMERICA_URL)
 soup = BeautifulSoup(response.text, 'html.parser')
 na_html = soup.find_all('table')[0]
 
-na_list = pd.read_html(str(na_html))
+na_list = pd.read_html(StringIO(na_html))
 na_df = pd.DataFrame(na_list[0]).head(NUM_PER_REGION)
 na_data = na_df.values.tolist()
 
@@ -69,7 +70,7 @@ response = requests.get(EUROPE_URL)
 soup = BeautifulSoup(response.text, 'html.parser')
 europe_html = soup.find_all('table')[5]
 
-europe_list = pd.read_html(str(europe_html))
+europe_list = pd.read_html(StringIO(europe_html))
 europe_df = pd.DataFrame(europe_list[0]).head(NUM_PER_REGION)
 europe_data = europe_df.values.tolist()
 
